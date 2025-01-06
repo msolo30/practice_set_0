8c15b269e582530d0c8984b90ddb5878e22cce75
Sophia <sophiam2702@tuckahoeschools.org>
2025-01-06 13:31:24 +0000

    initial commit

---
+++ hello.py
@@ -0,0 +1,29 @@
+"""#Ask for user name
+name = input("Whats your name? ")
+#remove whitespace
+name = name.strip()
+# Capatalize user name
+name.title()
+print(f"hello, {name}")"""
+
+"""#Ask for user name
+name = input("Whats your name? ").strip().title()
+print(f"hello, {name}")"""
+
+"""#Ask for name
+name = input("What's your name? ").strip().title()
+#Create function
+def hello():
+    print(f"Hello, {name}")
+#Call function
+hello()"""
+
+"""def main():
+    name= input ("What is your name? ").strip().title()
+    hello(name)
+
+def hello(user_name):
+    print(f"Hello, {user_name}")
+    
+main()"""
+
---
+++ indoor.py
@@ -0,0 +1,8 @@
+def quite():
+    name = input("What is your name? ").casefold()
+    hello(name)
+
+def hello(user_name):
+    print(f"Hello, {user_name}")
+
+quite()
